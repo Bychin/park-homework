375749ed1c408b3f8f8a41eb09ccd6da2fb579c6
Pavel <bychinpavel@gmail.com>
2017-03-10 17:50:57 +0300

    Pep8 check

--- ChampionsLeague.py
+++ ChampionsLeague.py
@@ -28,7 +28,7 @@ getch = _find_getch()
 
 
 def _exit():
-    os.system("clear") 
+    os.system("clear")
     print("Goodbye!")
     sys.exit()
 
@@ -36,7 +36,7 @@ def _exit():
 def _return_to_main():
     print("\nPress any key to return to the main menu...")
     in_ch = ord(getch())
-    if in_ch != None:
+    if in_ch is not None:
         os.system("clear")
     else:
         _exit()
@@ -52,10 +52,10 @@ def _stage_play(stg, teams_for_pairs, all_teams):
             print("\n---Final---\n")
             file.write("---Final---\n")
 
-        for i in range(stg): # for every 2 teams per stage
+        for i in range(stg):  # for every 2 teams per stage
 
-            left_side = teams_for_pairs.pop() # pick random 1st team
-            right_side = teams_for_pairs.pop() # pick random 2nd team
+            left_side = teams_for_pairs.pop()  # pick random 1st team
+            right_side = teams_for_pairs.pop()  # pick random 2nd team
 
             score_left = random.randint(0, 6)
             score_right = random.randint(0, 6)
@@ -68,13 +68,15 @@ def _stage_play(stg, teams_for_pairs, all_teams):
                 all_teams.remove(right_side)
             elif score_left < score_right:
                 all_teams.remove(left_side)
-            else: # Penalty shootout
+            else:  # Penalty shootout
                 pen_score_left = random.randint(0, 6)
                 pen_score_right = random.randint(0, 6)
                 while (pen_score_left == pen_score_right):
                     pen_score_right = random.randint(0, 6)
-                file.write("(Penalty: {} - {})\n".format(pen_score_left, pen_score_right))
-                print("(Penalty: {} - {})".format(pen_score_left, pen_score_right)) 
+                file.write("(Penalty: {} - {})\n".format(pen_score_left,
+                                                         pen_score_right))
+                print("(Penalty: {} - {})".format(pen_score_left,
+                                                  pen_score_right))
 
                 if pen_score_left > pen_score_right:
                     all_teams.remove(right_side)
@@ -88,14 +90,14 @@ def _stage_play(stg, teams_for_pairs, all_teams):
 def _show_results():
     print("\nDo you want to see the path of a particular team? [Y/N]")
     in_ch = ord(getch())
-    while in_ch == 89 or in_ch == 121: # (89 == 'y', 121 == 'Y')
+    while in_ch == 89 or in_ch == 121:  # (89 == 'y', 121 == 'Y')
         team_name = input("Enter team's name: ")
 
         file = open("history.txt", "r")
         stg = 8
-        key_check = False # Check for penalties
+        key_check = False  # Check for penalties
         for line in file:
-            if key_check == True and "Penalty" in line:
+            if key_check is True and "Penalty" in line:
                 print("{}".format(str(line[:-1])))
                 key_check = False
             else:
@@ -109,7 +111,7 @@ def _show_results():
                     print("Stage 1/{}: {}".format(stg, str(line[:-1])))
                 stg //= 2
                 key_check = True
-                
+
         if stg == 8:
             print("No such team!")
         print("Do you want to see more? [Y/N]")
@@ -130,7 +132,7 @@ if __name__ == "__main__":
 
         in_ch = ord(getch())
 
-        if in_ch == 49: # 1) Play! (49 == '1')
+        if in_ch == 49:  # 1) Play! (49 == '1')
             os.system("clear")
             random.seed()
             file = open("commands_list.txt", "r")
@@ -147,33 +149,32 @@ if __name__ == "__main__":
             stage = 8
             file = open("history.txt", "w")
 
-            _stage_play(stage, team_list, tmp_teams) # 1/8, 1/4, 1/2, Final
+            _stage_play(stage, team_list, tmp_teams)  # 1/8, 1/4, 1/2, Final
 
             print("\n{} won the competition!!!".format(team_list.pop()))
             file.write("\n{} is the winner!".format(tmp_teams.pop()))
             file.close()
 
             _show_results()
-            os.system("clear") 
+            os.system("clear")
 
-        elif in_ch == 50: # 2) Show all teams (50 == '2')
-            os.system("clear") 
+        elif in_ch == 50:  # 2) Show all teams (50 == '2')
+            os.system("clear")
             file = open("commands_list.txt", "r")
             for team in file:
                 print(team[:-1])
             file.close()
 
             _return_to_main()
-            
 
-        elif in_ch == 51: # 3) Change teams
-            os.system("clear") 
+        elif in_ch == 51:  # 3) Change teams
+            os.system("clear")
             print("Warning: next steps will delete all previous data!\nContinue? [Y/N]")
             in_ch = ord(getch())
 
-            if in_ch == 89 or in_ch == 121: # (89 == 'y', 121 == 'Y')
+            if in_ch == 89 or in_ch == 121:  # (89 == 'y', 121 == 'Y')
 
-                os.system("clear") 
+                os.system("clear")
                 print("Your must enter 16 teams in total:\n")
 
                 file = open("commands_list.txt", "w")
@@ -188,7 +189,7 @@ if __name__ == "__main__":
             else:
                 os.system("clear")
 
-        elif in_ch == 52: # 4) Exit (52 == '4')
+        elif in_ch == 52:  # 4) Exit (52 == '4')
             _exit()
 
         else:
