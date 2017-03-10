d4405e58e1c08f525836416a02893d1ab41164e2
Pavel Buchinchik <bychinpavel@gmail.com>
2017-03-10 15:03:55 +0300

    Functions were added
    _stage_play()
    _show_results()

--- ChampionsLeague.py
+++ ChampionsLeague.py
@@ -37,12 +37,12 @@ def _return_to_main():
     print("\nPress any key to return to the main menu...")
     in_ch = ord(getch())
     if in_ch != None:
-        os.system("clear") 
+        os.system("clear")
     else:
         _exit()
 
 
-def _stage_play(stg):
+def _stage_play(stg, teams_for_pairs, all_teams):
     while stg >= 1:
 
         if stg != 1:
@@ -52,10 +52,10 @@ def _stage_play(stg):
             print("\n---Final---\n")
             file.write("---Final---\n")
 
-        for i in range(stg):
+        for i in range(stg): # for every 2 teams per stage
 
-            left_side = team_list.pop()
-            right_side = team_list.pop()
+            left_side = teams_for_pairs.pop() # pick random 1st team
+            right_side = teams_for_pairs.pop() # pick random 2nd team
 
             score_left = random.randint(0, 6)
             score_right = random.randint(0, 6)
@@ -65,10 +65,10 @@ def _stage_play(stg):
             print("{} vs {}: {} - {}".format(left_side, right_side,
                                              score_left, score_right))
             if score_left > score_right:
-                tmp_teams.remove(right_side)
+                all_teams.remove(right_side)
             elif score_left < score_right:
-                tmp_teams.remove(left_side)
-            else:
+                all_teams.remove(left_side)
+            else: # Penalty shootout
                 pen_score_left = random.randint(0, 6)
                 pen_score_right = random.randint(0, 6)
                 while (pen_score_left == pen_score_right):
@@ -77,18 +77,49 @@ def _stage_play(stg):
                 print("(Penalty: {} - {})".format(pen_score_left, pen_score_right)) 
 
                 if pen_score_left > pen_score_right:
-                    tmp_teams.remove(right_side)
+                    all_teams.remove(right_side)
                 elif pen_score_left < pen_score_right:
-                    tmp_teams.remove(left_side)
+                    all_teams.remove(left_side)
 
-            team_list.update(tmp_teams)
-            stg //= 2
+        teams_for_pairs.update(all_teams)
+        stg //= 2
+
+
+def _show_results():
+    print("\nDo you want to see the path of a particular team? [Y/N]")
+    in_ch = ord(getch())
+    while in_ch == 89 or in_ch == 121: # (89 == 'y', 121 == 'Y')
+        team_name = input("Enter team's name: ")
+
+        file = open("history.txt", "r")
+        stg = 8
+        key_check = False # Check for penalties
+        for line in file:
+            if key_check == True and "Penalty" in line:
+                print("{}".format(str(line[:-1])))
+                key_check = False
+            else:
+                key_check = False
+            if team_name in str(line):
+                if stg == 1:
+                    print("Final: {}".format(str(line[:-1])))
+                elif stg == 0:
+                    print(str(line[:-1]))
+                else:
+                    print("Stage 1/{}: {}".format(stg, str(line[:-1])))
+                stg //= 2
+                key_check = True
+                
+        if stg == 8:
+            print("No such team!")
+        print("Do you want to see more? [Y/N]")
+        in_ch = ord(getch())
+        file.close()
 
 
 if __name__ == "__main__":
     os.system("clear")
     print("Welcome to the Champions League!\n")
-    
 
     while True:
         print("Select an option by entering a number: \n")
@@ -116,42 +147,13 @@ if __name__ == "__main__":
             stage = 8
             file = open("history.txt", "w")
 
-            _stage_play(stage)
+            _stage_play(stage, team_list, tmp_teams) # 1/8, 1/4, 1/2, Final
 
             print("\n{} won the competition!!!".format(team_list.pop()))
             file.write("\n{} is the winner!".format(tmp_teams.pop()))
             file.close()
 
-            print("\nDo you want to see the path of a particular team? [Y/N]")
-            in_ch = ord(getch())
-            while in_ch == 89 or in_ch == 121: # (89 == 'y', 121 == 'Y')
-                team_name = input("Enter team's name: ")
-
-                file = open("history.txt", "r")
-                tmp_tuple = tuple(file.readlines())
-                stg = 8
-                key_check = False #for penalties
-                for line in tmp_tuple:
-                    if key_check == True and "Penalty" in line:
-                        print("{}".format(str(line[:-1])))
-                        key_check = False
-                    else:
-                        key_check = False
-                    if team_name in str(line):
-                        if stg == 1:
-                            print("Final: {}".format(str(line[:-1])))
-                        elif stg == 0:
-                            print(str(line[:-1]))
-                        else:
-                            print("Stage 1/{}: {}".format(stg, str(line[:-1])))
-                        stg //= 2
-                        key_check = True
-                
-                if stg == 8:
-                    print("No such team!")
-                print("Do you want to see more? [Y/N]")
-                in_ch = ord(getch())
-                file.close()
+            _show_results()
             os.system("clear") 
 
         elif in_ch == 50: # 2) Show all teams (50 == '2')
@@ -184,7 +186,7 @@ if __name__ == "__main__":
                 _return_to_main()
 
             else:
-                os.system("clear") 
+                os.system("clear")
 
         elif in_ch == 52: # 4) Exit (52 == '4')
             _exit()
